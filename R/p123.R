e837425857a898b2e30a50ce3ea1ae59c7c7e8b0
Nagumanova01 <nagumanova_01@mail.ru>
2023-05-21 22:18:35 +0300

    aaa

--- R/p123.R
+++ R/p123.R
@@ -25,6 +25,9 @@ p3 = function(pi, sig){
   return(1 - p1(pi, sig) - p2(pi, sig))
 }
 
+destination = "C:\\Users\\Пользователь\\Desktop\\Диплом\\curs\\img\\p123_1.pdf"
+pdf(file = destination)
+
 ggplot() +
   xlim(c(0.8, 1)) + ylim(c(-0.1, 1)) +
   geom_function(fun = p1, args = list(sig = 0.8), colour = "#CD9B1D", lwd = 1, linetype = 1)+
@@ -35,6 +38,8 @@ ggplot() +
   annotate("text", x= 0.98 , y= 0.63 , label= "p2", col= "#00BFFF")+
   annotate("text", x= 0.9 , y= 0.37 , label= "p3", col= "#FF69B4")+
   ggtitle("sigma = 0.8, pi1 = 0.1, pi2 = 0.5, pi3 = pi")
+
+dev.off()
 
 #=================================================================
 
@@ -52,6 +57,9 @@ p2 = function(pi, sig){
   return ((1+t1*t3)/((t2-t1)*(t2-t3)))
 }
 
+destination = "C:\\Users\\Пользователь\\Desktop\\Диплом\\curs\\img\\p123_2.pdf"
+pdf(file = destination)
+
 ggplot() +
   xlim(c(0.11,0.89)) + ylim(c(-0.1,1)) +
   geom_function(fun = p1, args = list(sig = 0.8), colour = "#CD9B1D", lwd = 1, linetype = 1)+
@@ -62,6 +70,8 @@ ggplot() +
   annotate("text", x= 0.15 , y= -5 , label= "p2", col= "#00BFFF")+
   annotate("text", x= 0.25 , y= 0.51 , label= "p3", col= "#FF69B4")+
   ggtitle("sigma = 0.8, pi1 = 0.1, pi2 = pi, pi3 = 0.9")
+
+dev.off()
 
 #===========================================
 
@@ -78,7 +88,10 @@ p2 = function(pi, sig){
   t3 = t(0.9, sig)
   return ((1+t1*t3)/((t2-t1)*(t2-t3)))
 }
-  
+
+destination = "C:\\Users\\Пользователь\\Desktop\\Диплом\\curs\\img\\p123_3.pdf"
+pdf(file = destination)
+
 ggplot() +
   xlim(c(0,0.49)) + ylim(c(-0.1,1)) +
   geom_function(fun = p1, args = list(sig = 0.8), colour = "#CD9B1D", lwd = 1, linetype = 1)+
@@ -90,6 +103,7 @@ ggplot() +
   annotate("text", x= 0.2 , y= 0.53 , label= "p3", col= "#FF69B4")+
   ggtitle("sigma = 0.8, pi1 = pi, pi2 = 0.5, pi3 = 0.9")
 
+dev.off()
 #========================================
 
 p1 = function(pi, sig){
@@ -232,7 +246,7 @@ for (i in v){
 
 df <- data.frame(x = v, y = s1, z = s2)
 
-destination = "C:\\Users\\Пользователь\\Desktop\\Диплом\\curs\\img\\sigma_12.pdf"
+destination = "C:\\Users\\????????????\\Desktop\\??????\\curs\\img\\sigma_12.pdf"
 pdf(file = destination)
 
 ggplot(df, aes (x = x)) +
