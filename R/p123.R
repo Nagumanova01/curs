cb465dc072fef5001cce0a5766d620bdb1c8fc6e
Nagumanova01 <nagumanova_01@mail.ru>
2023-05-22 21:26:03 +0300

    новый документ

--- R/p123.R
+++ R/p123.R
@@ -222,8 +222,9 @@ f_s2 = function(pi){
   s_lim = 0
   for (i in s){
     an = exp(-i*i)-exp(i*qnorm(0.1)-i*i/2)-exp(i*qnorm(pi)-i*i/2)+exp(i*i)
+    print(an)
     if (is.na(an)){
-      #print('Missing')
+      print('Missing')
       #an=0
     }
     if (an>0){
@@ -260,4 +261,93 @@ ggplot(df, aes (x = x)) +
              lwd = 1,      
              linetype = 1) +
   labs(x = "pi", y = "sigma")
+dev.off()
+
+#==================
+#pi2 = 0.7
+library(ggplot2)
+
+t = function(pi, sig){
+  v1 = exp(sig*qnorm(pi)-sig*sig/2)-1
+  v2 = sqrt(exp(sig*sig)-1)
+  return(v1/v2)
+}
+
+p2 = function(pi, sig){
+  t1 = t(1-pi, sig)
+  t2 = t(0.5, sig)
+  t3 = t(pi, sig)
+  return ((1+t1*t3)/((t2-t1)*(t2-t3)))
+}
+
+p2_2 = function(pi, sig){
+  t1 = t(0.1, sig)
+  t2 = t(0.5, sig)
+  t3 = t(pi, sig)
+  return ((1+t1*t3)/((t2-t1)*(t2-t3)))
+}
+
+f_s1 = function(pi){
+  s=seq(0.01,2, by = 0.01)
+  s_lim = 0
+  for (i in s){
+    an = p2(pi, i)
+    #print(an)
+    if (is.na(an)){
+      print('Missing1')
+      #an=0
+    }
+    if (an<0){
+      s_lim = i
+      break
+    }
+  }
+  return(s_lim)
+}
+
+f_s2 = function(pi){
+  s=seq(0.01,2, by = 0.01)
+  s_lim = 0
+  for (i in s){
+    an = p2_2(pi,i)
+    #print(an)
+    if (is.na(an)){
+      print('Missing2')
+      #an=0
+    }
+    if (an<0){
+      s_lim = i
+      break
+    }
+  }
+  return(s_lim)
+}
+
+v = seq(0.51,0.99, by = 0.01)
+s1 = rep(0, 49)
+s2 = rep(0, 49)
+j = 1
+for (i in v){
+  s1[j] = f_s1(i)
+  s2[j] = f_s2(i)
+  j = j+1
+}
+
+df <- data.frame(x = v, y = s1, z = s2)
+
+destination = "C:\\Users\\Пользователь\\Desktop\\Диплом\\curs\\img\\sigma_12_new.pdf"
+pdf(file = destination)
+
+ggplot(df, aes (x = x)) +
+  xlim(c(0.5,1))+
+  geom_line( aes (y = y, color = 'pi3 = 1-pi1 = pi'), 
+             color = 2,    
+             lwd = 1,      
+             linetype = 1) + 
+  geom_line( aes (y = z, color = 'pi1 = 0.1, pi3 = pi'), 
+             color = 3,    
+             lwd = 1,      
+             linetype = 1) +
+  labs(x = "pi", y = "sigma")
+
 dev.off()
--- R/param_new.R
+++ R/param_new.R
@@ -6,15 +6,20 @@ library(ggrepel)
 library(readxl)
 
 m<-function(x){
-  return(abs(exp(x*x/2)-0.305*exp(-1.28*x)-0.39-0.305*exp(1.28*x))/(exp(x*x/2)))
+  return((exp(x*x/2)-0.305*exp(-1.28*x)-0.39-0.305*exp(1.28*x))/(exp(x*x/2)))
 }
 
 s<-function(x){
-  return(abs(exp(x*x)*(exp(x^2)-1)-0.305*(exp(-1.28*x))^2-0.39-0.305*(exp(1.28*x))^2+(0.305*exp(-1.28*x)+0.39+0.305*exp(1.28*x))^2)/(exp(x*x)*(exp(x^2)-1)))
+  return((exp(x*x)*(exp(x^2)-1)-0.305*(exp(-1.28*x))^2-0.39-0.305*(exp(1.28*x))^2+(0.305*exp(-1.28*x)+0.39+0.305*exp(1.28*x))^2)/(exp(x*x)*(exp(x^2)-1)))
 }
+
+
+destination = "C:\\Users\\Пользователь\\Desktop\\Диплом\\curs\\img\\par_new.pdf"
+pdf(file = destination)
 
 ggplot() +
   xlim(c(0, 5)) +
+  labs(x = "sigma", y = "")+
   geom_function(aes(color = "отн. ошибка мат.ож."), fun = m,
                 colour = "red",
                 lwd = 1,
@@ -22,7 +27,11 @@ ggplot() +
   geom_function(aes(color = "отн. ошибка дисперсии"), fun = s,
                 colour = "blue",
                 lwd = 1,
-                linetype = 1)
+                linetype = 1)+
+  annotate("text", x= 0.75 , y= 0.98 , label= "отн. ошибка дисперсии", col=" blue")+
+  annotate("text", x= 4 , y= 0.75 , label= "отн. ошибка мат.ожидания", col=" red")
+
+dev.off()
   
 
 ggplot() +
